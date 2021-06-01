396f9cb864c5c5878c47956c1147970fcf93bf4a
Ncesecrede <francescopm95@gmail.com>
2021-06-01 15:37:08 +0100

    FizzBuzz

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 
 
 for (var x = 1; x <= 100; x++){
-    document.getElementById("numeri").innerHTML = x + '</br>';
+        document.getElementById("numeri").innerHTML += x + "</br>";
+     if ( x % 3 == 0  &&  x % 5 == 0){
+        document.getElementById("numeri").innerHTML += "FizzBuzz" + "</br>";
+     }else if ( x % 3 == 0){
+         document.getElementById("numeri").innerHTML += "Fizz" + "</br>";
+     }else if ( x % 5 == 0){
+         document.getElementById("numeri").innerHTML += "Buzz" + "</br>";
+     }
 }
+
